06e65367043cd686d40242a33c05c84d047d82df
Jakub Dudarewicz <Jakub.Dudarewicz@aptiv.com>
2024-08-13 16:49:12 +0200

    Add basic local lottery

--- src/config.rs
+++ src/config.rs
@@ -1,37 +1,14 @@
-use config::Config;
-use std::{collections::HashMap, net::{SocketAddrV4, Ipv4Addr}, str::FromStr};
+use std::collections::HashMap;
 
-const CONFIG_PORT : &str = "port";
-const CONFIG_ADDRESS : &str = "address";
-const CONFIG_HOST : &str = "host_dir";
-const CONFIG_HOMEPAGE : &str = "home";
+use config::Config;
+
 const CONFIG_NAME_SOURCE : &str = "name_source";
 
 pub struct LotteryConfig {
-    pub socket : SocketAddrV4,
-    pub host_dir : String,
-    pub homepage : String,
     pub name_source : String,
 }
 
 impl LotteryConfig {
-    fn create_socket(map: &HashMap<String, String>) -> SocketAddrV4 {
-        /* get address from config */
-        let address: Ipv4Addr = Ipv4Addr::from_str(
-            map
-                .get(CONFIG_ADDRESS)
-                .expect("No address found in {CONFIG_PATH}")
-            ).expect("Invalid IP address");
-
-        /* get port from config */
-        let port = u16::from_str(
-            map
-                .get(CONFIG_PORT)
-                .expect("No port found in {CONFIG_PATH}")
-            ).expect("Invalid port value");
-        
-        SocketAddrV4::new(address, port)
-    }
 
     pub fn new(config_path: &str) -> LotteryConfig {
         /* Create a config builder, based on file from CONFIG_PATH */
@@ -44,45 +21,9 @@ impl LotteryConfig {
             .expect("Invalid config file");
 
         LotteryConfig {
-            socket : Self::create_socket(&config_map),
-            host_dir : config_map.get(CONFIG_HOST)
-                                 .expect("Host directory not configured")
-                                 .to_string(),
-            homepage : config_map.get(CONFIG_HOMEPAGE)
-                                 .expect("Homepage not configured")
-                                 .to_string(),
             name_source : config_map.get(CONFIG_NAME_SOURCE)
                                     .expect("Name source not configured")
                                     .to_string(),
         }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn socket_test_localhost() {
-        let mut map : HashMap<String, String> = HashMap::new();
-        map.insert(CONFIG_PORT.to_string(), "3000".to_string());
-        map.insert(CONFIG_ADDRESS.to_string(), "127.0.0.1".to_string());
-
-        let socket = LotteryConfig::create_socket(&map);
-
-        assert_eq!(socket.ip().octets(), [127, 0, 0, 1]);
-        assert_eq!(socket.port(), 3000);
-    }
-
-    #[test]
-    fn socket_test_lan() {
-        let mut map : HashMap<String, String> = HashMap::new();
-        map.insert(CONFIG_PORT.to_string(), "4000".to_string());
-        map.insert(CONFIG_ADDRESS.to_string(), "192.168.1.100".to_string());
-
-        let socket = LotteryConfig::create_socket(&map);
-
-        assert_eq!(socket.ip().octets(), [192, 168, 1, 100]);
-        assert_eq!(socket.port(), 4000);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,70 @@
-use hyper::server::conn::http1;
-use hyper_util::rt::TokioIo;
-use tokio::net::TcpListener;
+use names::Provider;
+use rand;
+use std::sync::{Arc, Mutex};
 
 mod config;
-mod http_service;
 mod names;
 
 const CONFIG_PATH: &str = "config.toml";
 
-#[tokio::main]
-pub async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let config = config::LotteryConfig::new(CONFIG_PATH);
+slint::include_modules!();
 
-    let html_provider = std::sync::Arc::new(names::html::HtmlProvider::new(&config.name_source));
+fn names_to_string(names: &Vec<String>) -> String {
+    names.join("\n").into()
+}
 
-    let service = http_service::LotteryService::new(&http_service::LotteryServiceConfig {
-        host_prefix: config.host_dir,
-        homepage: config.homepage,
-        name_provider: html_provider,
+fn main() -> Result<(), slint::PlatformError> {
+    let config: config::LotteryConfig = config::LotteryConfig::new(CONFIG_PATH);
+
+    let provider: names::html::HtmlProvider = crate::names::html::HtmlProvider::new(&config.name_source);
+    let names =
+        Arc::new(
+            Mutex::new(
+                provider.get_names().unwrap()));
+
+    let ui = AppWindow::new()?;
+    
+    ui.set_list(names_to_string(&names.lock().unwrap()).into());
+
+    ui.set_listFile(config.name_source.into());
+
+    ui.on_draw_person({
+        let ui_handle = ui.as_weak();
+        let names = names.clone();
+        move || {
+            let ui = ui_handle.unwrap();
+
+            let mut names = names.lock().unwrap();
+
+            if names.len() > 1 {
+                let random_index = rand::random::<usize>() % names.len();
+                let name = names[random_index].clone();
+                names.remove(random_index);
+    
+                ui.set_list(names_to_string(&names).into());
+                ui.set_winner(name.into());
+            } else {
+                let mut winner_text: String = "WINNER: ".to_string();
+                winner_text += &names[0].clone();
+                ui.set_winner(winner_text.into());
+            }
+        }
     });
 
-    // Bind to the port and listen for incoming TCP connections
-    let listener = TcpListener::bind(config.socket).await?;
-    println!("Listening on http://{}", config.socket);
-    loop {
-        // When an incoming TCP connection is received grab a TCP stream for
-        // client<->server communication.
-        //
-        // Note, this is a .await point, this loop will loop forever but is not a busy loop. The
-        // .await point allows the Tokio runtime to pull the task off of the thread until the task
-        // has work to do. In this case, a connection arrives on the port we are listening on and
-        // the task is woken up, at which point the task is then put back on a thread, and is
-        // driven forward by the runtime, eventually yielding a TCP stream.
-        let (tcp, _) = listener.accept().await?;
-        // Use an adapter to access something implementing `tokio::io` traits as if they implement
-        // `hyper::rt` IO traits.
-        let io = TokioIo::new(tcp);
-        let service_clone = service.clone();
+    ui.on_restart({
+        let ui_handle = ui.as_weak();
+        let names = names.clone();
+        move || {
+            let ui = ui_handle.unwrap();
 
-        // Spin up a new task in Tokio so we can continue to listen for new TCP connection on the
-        // current task without waiting for the processing of the HTTP1 connection we just received
-        // to finish
-        tokio::task::spawn(async move {
-            // Handle the connection from the client using HTTP1 and pass any
-            // HTTP requests received on that connection to the `hello` function
-            if let Err(err) = http1::Builder::new()
-                .serve_connection(io, service_clone)
-                .await
-            {
-                println!("Error serving connection: {:?}", err);
-            }
-        });
-    }
+            let mut names = names.lock().unwrap();
+
+            *names = provider.get_names().unwrap();
+
+            ui.set_list(names_to_string(&names).into());
+            ui.set_winner("".into());
+        }
+    });
+
+    ui.run()
 }
